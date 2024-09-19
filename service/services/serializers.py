381a5ec2bf870580aefe667d5f0e79808fdc41b6
MatveyMahlyshev <matthewfootballer11@gmail.com>
2024-09-19 22:25:46 +0300

    test

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
 
 class SubscriptionSerializer(serializers.ModelSerializer):
-    pass
+    client_name = serializers.CharField(source='client.company_name')
+    email = serializers.CharField(source='client.user.email')
+    
