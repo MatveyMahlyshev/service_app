224e9857210ef3dad74ce1386885f2388c1f4335
MatveyMahlyshev <matthewfootballer11@gmail.com>
2024-09-19 22:29:46 +0300

    testing with venv

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -3,4 +3,3 @@ from rest_framework import serializers
 class SubscriptionSerializer(serializers.ModelSerializer):
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
-    
